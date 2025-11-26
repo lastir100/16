0af66b89fd445f2d83ba09d819e9ed07b360d768
Aleksandr.Minin <lastir@yandex.ru>
2025-11-26 22:53:43 +0300

    modified hw-01.md adding answers in task 2 created tf files in ./task2 and ./task2/vm

---
+++ 01/task2/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.6.1"
+    }
+  }
+  required_version = ">=1.12.0" /*Многострочный комментарий.
+ Требуемая версия terraform */
+}
+provider "docker" {
+  host     = "ssh://ubuntu@158.160.109.163:22"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
+
+resource "random_password" "root_password" {
+  length      = 16
+  special     = true
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+  min_special = 1
+}
+
+resource "random_password" "user_password" {
+  length      = 16
+  special     = true
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+  min_special = 1
+}
+
+
+resource "docker_image" "mysql" {
+  name         = "mysql:8"
+
+}
+
+resource "docker_container" "mysql" {
+  image = docker_image.mysql.image_id
+  name  = "mysql"
+
+  env = [ 
+    "MYSQL_ROOT_PASSWORD=${random_password.root_password.result}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${random_password.user_password.result}",
+    "MYSQL_ROOT_HOST=%"
+  ]
+
+
+  ports {
+    internal = 3306
+    external = 3306
+  }
+}
+
